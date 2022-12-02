e4d411f5cd01d5e67febabfa982d916b0b891d73
misracis2 <misracis@gmail.com>
2022-12-02 16:58:06 +0900

    커밋

---
+++ src/main/java/com/example/practice_myselectshop/PracticeMyselectshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice_myselectshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeMyselectshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeMyselectshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practice_myselectshop/PracticeMyselectshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice_myselectshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeMyselectshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
