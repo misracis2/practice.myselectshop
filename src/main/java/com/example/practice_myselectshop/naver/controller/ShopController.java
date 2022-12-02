4bc4ada52394a0977dfbbeb0ba7487e6c1549fdf
misracis2 <misracis@gmail.com>
2022-12-02 20:06:20 +0900

    1-1강 내용 적용

--- src/main/java/com/example/practice_myselectshop/naver/controller/ShopController.java
+++ src/main/java/com/example/practice_myselectshop/naver/controller/ShopController.java
@@ -1,2 +1,16 @@
-package com.example.practice_myselectshop.naver.controller;public class ShopController {
+package com.example.practice_myselectshop.naver.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/api")
+public class ShopController {
+
+    @GetMapping("/shop")
+    public ModelAndView shop() {
+        return new ModelAndView("index");
+    }
 }
--- src/main/java/com/example/practice_myselectshop/naver/service/NaverApiService.java
+++ src/main/java/com/example/practice_myselectshop/naver/service/NaverApiService.java
@@ -1,2 +1,51 @@
-package com.example.practice_myselectshop.naver.service;public class NaverApiService {
+package com.example.practice_myselectshop.naver.service;
+
+import com.example.practice_myselectshop.naver.dto.ItemDto;
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Service
+public class NaverApiService {
+    //네이버 API와 소통하는 코드, naver developer에서 정해준것을 가져와 쓰면 된다
+    public List<ItemDto> searchItems(String query) {
+        RestTemplate rest = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Naver-Client-Id", "v4iWn0oN3z9z0o_x6KeE");
+        headers.add("X-Naver-Client-Secret", "GVavlDjuIo");
+        String body = "";
+
+        HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
+        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?display=15&query=" + query , HttpMethod.GET, requestEntity, String.class);
+
+        HttpStatus httpStatus = responseEntity.getStatusCode();
+        int status = httpStatus.value();
+        log.info("NAVER API Status Code : " + status);
+
+        String response = responseEntity.getBody();
+
+        return fromJSONtoItems(response);
+    }
+    //naver api에서 가져온 데이터를 dto로 변환하는 과정
+    public List<ItemDto> fromJSONtoItems(String response) {
+
+        JSONObject rjson = new JSONObject(response);
+        JSONArray items  = rjson.getJSONArray("items");//json형태 데이터에서 필요한 items 데이터를 배열로 변환
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for (int i=0; i<items.length(); i++) {
+            JSONObject itemJson = items.getJSONObject(i);
+            ItemDto itemDto = new ItemDto(itemJson);
+            itemDtoList.add(itemDto);
+        }
+
+        return itemDtoList;
+    }
 }
